6961dba10e8810261634233eaf8ef94c4070d109
Peter Rigo <boys4849@gmail.com>
2021-02-26 07:22:30 +0100

    style of web page

--- index.php
+++ index.php
@@ -4,67 +4,78 @@
     <title>Cvičenie 1</title>
     <meta charset="utf-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="css/stylesheet.css">
     <link rel="stylesheet" href="css/table.css">
 </head>
 <body>
-<pre>
-
-</pre>
-
     <header>
-        <span class="welcome-header">Vitajte na tejto stránke</span>
+        <span class="welcome-header">MachShare</span> <img src="images/image.jpg" class="rounded" alt="...">
     </header>
-    <div class="border">
+
+    <div class="container border">
         <main>
-                <table>
-                    <tr>
-                        <th>Názov súboru</th>
-                        <th>Veľkosť</th>
-                        <th>Dátum nahrania</th>
-                    </tr>
-                    <?php
-                    $directoryArray = scandir('/home/xrigo/public_html/cvicenia/cvicenie1/files', 1);
-                    $directoryArray = array_diff($directoryArray, array('.', '..'));
-                    foreach ($directoryArray as $file){
-                            echo "<tr>
-                                <td>
-                                {$file}
-                                </td>
-                                <td>"
-                                . filesize("files/" . $file) .
-                                "</td>
-                                <td>
-                                
-                                </td>
-                                </tr>";
-                    }
-                    ?>
-                </table>
-            <div class="form-action">
+            <div class="row">
+                <div class="col table" >
+                    <h2>Uložené súbory</h2>
+                    <table class="container" id="table" style="overflow: scroll">
+                        <tr>
+                            <th id="0" onclick="sortTable(0,'text')" >Názov súboru </th>
+                            <th id="1"  onclick="sortTable(1,'number')">Veľkosť </th>
+                            <th id="2" onclick="sortTable(2,'text')">Dátum nahrania </th>
+                        </tr>
+                        <?php
+                        $directoryArray = scandir('/home/xrigo/public_html/cvicenia/cvicenie1/files', 1);
+                        $directoryArray = array_diff($directoryArray, array('.', '..'));
+                        foreach ($directoryArray as $file){
+                            $date = date_create();
+                                echo "<tr>
+                                    <td>
+                                    {$file}
+                                    </td>
+                                    <td>"
+                                    . filesize("files/" . $file)  . ' Bytes' .
+                                    "</td>
+                                    <td>"
+                                    .
+                                    date_format($date, 'U = Y-m-d H:i:s') . "\n"
+                                    ."
+                                    </td>
+                                    </tr>";
+                        }
+                        ?>
+                    </table>
+                </div>
+            <div class="col form">
+                <h2>Nahraj súbor</h2>
                 <form id="profileData"  enctype="multipart/form-data">
                     <div class="form-group">
-                        <label for="filename">Filename</label>
-                        <input name="filename" type="text" id="filename" class="form-control" placeholder="File name">
+                        <label for="filename">Názov súboru</label>
+                        <input name="filename" type="text" id="filename" class="form-control" placeholder="Názov súboru">
                     </div>
                     <span class="choose-action">Vyberte obrázok na nahranie na server:</span>
                     <input class="fileToUpload" type="file" name="upfile" id="upfile">
                     <div class="input-button">
-                        <button type="button" id="submit" class="btn btn-outline-primary">Submit</button>
+                        <button type="button" id="submit" class="btn btn-outline-primary">Nahrať</button>
                     </div>
                 </form>
-            </div>
-            <div class="about">
-                <h1>
-                    Popis:
-                </h1>
-                <p class="about-text">
-                    Táto stránka slúži na uploadnutie vašich súborov pre cvičenie 1 z predmetu Webtech2.
-                    Autor: <strong>Peter Rigo</strong>
-                </p>
+                <div class="about">
+                    <h1>
+                        Popis:
+                    </h1>
+                    <p class="about-text">
+                        Táto stránka slúži na uploadnutie vašich súborov pre cvičenie 1 z predmetu Webtech2.
+                        Autor: <strong>Peter Rigo</strong>
+                    </p>
+                </div>
             </div>
         </main>
     </div>
+    <footer class="footer">
+        ©PeterRigoDevelopment
+    </footer>
     <script src="js/javascript.js"></script>
+    <script src="js/sortTable.js"></script>
 </body>
 </html>
---
+++ js/sortTable.js
@@ -0,0 +1,98 @@
+function sortTable(n,type) {
+    let table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0, img, header;
+    table = document.getElementById("table");
+    switching = true;
+    // Set the sorting direction to ascending:
+    dir = "asc";
+    /* Make a loop that will continue until
+    no switching has been done: */
+    while (switching) {
+        // Start by saying: no switching is done:
+        switching = false;
+        rows = table.rows;
+        /* Loop through all table rows (except the
+        first, which contains table headers): */
+        for (i = 1; i < (rows.length - 1); i++) {
+            // Start by saying there should be no switching:
+            shouldSwitch = false;
+            /* Get the two elements you want to compare,
+            one from current row and one from the next: */
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+            if(type === "text") {
+                /* Check if the two rows should switch place,
+                based on the direction, asc or desc: */
+                if (dir === "asc") {
+                    if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                        // If so, mark as a switch and break the loop:
+                        shouldSwitch = true;
+                        break;
+                    }
+                } else if (dir === "desc") {
+                    if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                        // If so, mark as a switch and break the loop:
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+            }
+            else if(type === "number"){
+                x = x.innerHTML.split(" ");
+                x = x[0];
+                y = y.innerHTML.split(" ");
+                y = y[0];
+                //check if the two rows should switch place:
+                if (dir === "asc") {
+                    if (Number(x) > Number(y)) {
+                        //if so, mark as a switch and break the loop:
+                        shouldSwitch = true;
+                        break;
+                    }
+
+                }
+                else if (dir === "desc") {
+                    if (Number(x) < Number(y)) {
+                        //if so, mark as a switch and break the loop:
+                        shouldSwitch = true;
+                        break;
+                    }
+                }
+            }
+        }
+        if (shouldSwitch) {
+            /* If a switch has been marked, make the switch
+            and mark that a switch has been done: */
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            // Each time a switch is done, increase this count by 1:
+            switchcount ++;
+        } else {
+            /* If no switching has been done AND the direction is "asc",
+            set the direction to "desc" and run the while loop again. */
+            if (switchcount === 0 && dir === "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
+    header = document.getElementById(n);
+    if(dir === "desc"){
+        img = '<img class="sorting-image" src="icons/down.png" alt="a-z">'
+    }else{
+        img = '<img class="sorting-image" src="icons/up.png" alt="a-z">'
+    }
+    if(n===0){
+        header.innerHTML = 'Názov súboru ' + img;
+        document.getElementById("1").innerHTML = 'Veľkosť';
+        document.getElementById("2").innerHTML = 'Dátum nahrania';
+    }else if(n===1){
+        header.innerHTML = 'Veľkosť ' + img;
+        document.getElementById("0").innerHTML = 'Názov súboru';
+        document.getElementById("2").innerHTML = 'Dátum nahrania';
+    }else if(n === 2){
+        header.innerHTML = 'Dátum nahrania  ' + img;
+        document.getElementById("0").innerHTML = 'Názov súboru';
+        document.getElementById("1").innerHTML = 'Veľkosť';
+    }
+
+}
